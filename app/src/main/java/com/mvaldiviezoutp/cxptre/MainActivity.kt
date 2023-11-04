0c463ccfa9df009c2ab2af483f5e8af1456ad7c0
michael_abx <mavaldiviezo.abx@gmail.com>
2023-11-04 13:07:02 -0500

    Initial commit

---
+++ app/src/main/java/com/mvaldiviezoutp/cxptre/MainActivity.kt
@@ -0,0 +1,62 @@
+package com.mvaldiviezoutp.cxptre
+
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Button
+import android.widget.ImageView
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.NonNull
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+
+class MainActivity : AppCompatActivity() {
+    private val CAMERA_REQUEST_CODE = 101
+    private val GALLERY_REQUEST_CODE = 102 // Agrega esta constante
+
+    private val takePicture = registerForActivityResult(ActivityResultContracts.TakePicturePreview()) { result ->
+        if (result != null) {
+            val imageView: ImageView = findViewById(R.id.img)
+            imageView.setImageBitmap(result)
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val openCameraButton: Button = findViewById(R.id.open_camera)
+        val openGalleryButton: Button = findViewById(R.id.open_gallery)
+
+        openCameraButton.setOnClickListener {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+                takePicture.launch(null)
+            } else {
+                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), CAMERA_REQUEST_CODE)
+            }
+        }
+
+        openGalleryButton.setOnClickListener {
+            openGallery()
+        }
+    }
+
+    fun openGallery() {
+        val galleryIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        startActivityForResult(galleryIntent, GALLERY_REQUEST_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == GALLERY_REQUEST_CODE && resultCode == RESULT_OK && data != null) {
+            val selectedImage: Uri? = data.data
+            val imageView: ImageView = findViewById(R.id.img)
+            imageView.setImageURI(selectedImage)
+        }
+    }
+}
